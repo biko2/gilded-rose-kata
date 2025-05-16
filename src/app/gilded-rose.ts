b378c73926f9b889eeaaf1ddb81280b93c87b5ee
Endika Orube <endikaorve@gmail.com>
2025-05-16 11:09:56 +0200

    refactor: extrae actualización de calidad tras expiración a función independiente

--- src/app/gilded-rose.ts
+++ src/app/gilded-rose.ts
@@ -17,24 +17,7 @@ export class GildedRose {
     this.items.forEach((item) => {
       updateQuality1(item);
       updateSellIn(item);
-
-      if (item.sellIn < 0) {
-        if (item.name != "Aged Brie") {
-          if (item.name != "Backstage passes to a TAFKAL80ETC concert") {
-            if (item.quality > 0) {
-              if (item.name != "Sulfuras, Hand of Ragnaros") {
-                item.quality = item.quality - 1;
-              }
-            }
-          } else {
-            item.quality = item.quality - item.quality;
-          }
-        } else {
-          if (item.quality < 50) {
-            item.quality = item.quality + 1;
-          }
-        }
-      }
+      updateQualityAfterExpiration(item);
     });
 
     return this.items;
@@ -73,5 +56,25 @@ const updateQuality1 = (item: Item): void => {
 const updateSellIn = (item: Item): void => {
   if (item.name != "Sulfuras, Hand of Ragnaros") {
     item.sellIn = item.sellIn - 1;
+  }
+};
+
+const updateQualityAfterExpiration = (item: Item): void => {
+  if (item.sellIn < 0) {
+    if (item.name != "Aged Brie") {
+      if (item.name != "Backstage passes to a TAFKAL80ETC concert") {
+        if (item.quality > 0) {
+          if (item.name != "Sulfuras, Hand of Ragnaros") {
+            item.quality = item.quality - 1;
+          }
+        }
+      } else {
+        item.quality = item.quality - item.quality;
+      }
+    } else {
+      if (item.quality < 50) {
+        item.quality = item.quality + 1;
+      }
+    }
   }
 };
