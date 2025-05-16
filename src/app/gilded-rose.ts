34efb9fc22776c92ebdbd38dfd351d5e7af1c2ac
Endika Orube <endikaorve@gmail.com>
2025-05-16 11:36:34 +0200

    refactor: elimina updateItem y mueve lógica al forEach

--- src/app/gilded-rose.ts
+++ src/app/gilded-rose.ts
@@ -15,71 +15,67 @@ export class GildedRose {
 
   updateQuality() {
     this.items.forEach((item) => {
-      updateItem(item);
+      if (item.name === "Sulfuras, Hand of Ragnaros") {
+        return;
+      }
+
+      if (item.name === "Aged Brie") {
+        item.sellIn = item.sellIn - 1;
+
+        if (item.quality < 50) {
+          item.quality = item.quality + 1;
+        }
+
+        if (item.sellIn < 0) {
+          if (item.quality < 50) {
+            item.quality = item.quality + 1;
+          }
+        }
+
+        return;
+      }
+
+      if (item.name === "Backstage passes to a TAFKAL80ETC concert") {
+        item.sellIn = item.sellIn - 1;
+
+        if (item.quality < 50) {
+          item.quality = item.quality + 1;
+
+          if (item.sellIn < 10) {
+            if (item.quality < 50) {
+              item.quality = item.quality + 1;
+            }
+          }
+
+          if (item.sellIn < 5) {
+            if (item.quality < 50) {
+              item.quality = item.quality + 1;
+            }
+          }
+        }
+
+        if (item.sellIn < 0) {
+          item.quality = item.quality - item.quality;
+        }
+
+        return;
+      }
+
+      item.sellIn = item.sellIn - 1;
+
+      if (item.quality <= 0) {
+        return;
+      }
+
+      item.quality = item.quality - 1;
+
+      if (item.sellIn < 0) {
+        if (item.quality > 0) {
+          item.quality = item.quality - 1;
+        }
+      }
     });
 
     return this.items;
   }
 }
-
-const updateItem = (item: Item): void => {
-  if (item.name === "Sulfuras, Hand of Ragnaros") {
-    return;
-  }
-
-  if (item.name === "Aged Brie") {
-    item.sellIn = item.sellIn - 1;
-
-    if (item.quality < 50) {
-      item.quality = item.quality + 1;
-    }
-
-    if (item.sellIn < 0) {
-      if (item.quality < 50) {
-        item.quality = item.quality + 1;
-      }
-    }
-
-    return;
-  }
-
-  if (item.name === "Backstage passes to a TAFKAL80ETC concert") {
-    item.sellIn = item.sellIn - 1;
-
-    if (item.quality < 50) {
-      item.quality = item.quality + 1;
-
-      if (item.sellIn < 10) {
-        if (item.quality < 50) {
-          item.quality = item.quality + 1;
-        }
-      }
-
-      if (item.sellIn < 5) {
-        if (item.quality < 50) {
-          item.quality = item.quality + 1;
-        }
-      }
-    }
-
-    if (item.sellIn < 0) {
-      item.quality = item.quality - item.quality;
-    }
-
-    return;
-  }
-
-  item.sellIn = item.sellIn - 1;
-
-  if (item.quality <= 0) {
-    return;
-  }
-
-  item.quality = item.quality - 1;
-
-  if (item.sellIn < 0) {
-    if (item.quality > 0) {
-      item.quality = item.quality - 1;
-    }
-  }
-};
