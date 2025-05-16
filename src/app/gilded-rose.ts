c5415e16e56a9507602c0810adc066eb6845d69c
Endika Orube <endikaorve@gmail.com>
2025-05-16 11:39:15 +0200

    refactor: simplifica validación de calidad máxima en Aged Brie

--- src/app/gilded-rose.ts
+++ src/app/gilded-rose.ts
@@ -22,14 +22,14 @@ export class GildedRose {
       if (item.name === "Aged Brie") {
         item.sellIn = item.sellIn - 1;
 
-        if (item.quality < 50) {
+        item.quality = item.quality + 1;
+
+        if (item.sellIn < 0) {
           item.quality = item.quality + 1;
         }
 
-        if (item.sellIn < 0) {
-          if (item.quality < 50) {
-            item.quality = item.quality + 1;
-          }
+        if (item.quality > 50) {
+          item.quality = 50;
         }
 
         return;
