c4cf14bb8b4766b44c67d332824730add04d463a
Endika Orube <endikaorve@gmail.com>
2025-05-16 11:52:06 +0200

    refactor: crea clase abstracta ItemStrategy para patrón strategy

--- src/app/gilded-rose.ts
+++ src/app/gilded-rose.ts
@@ -6,6 +6,41 @@ export class Item {
   ) {}
 }
 
+export abstract class ItemStrategy {
+  protected item: Item;
+
+  constructor(item: Item) {
+    this.item = item;
+  }
+
+  abstract update(): void;
+
+  protected ensureQualityInRange(): void {
+    if (this.item.quality > 50) {
+      this.item.quality = 50;
+    }
+    if (this.item.quality < 0) {
+      this.item.quality = 0;
+    }
+  }
+
+  protected decreaseSellIn(): void {
+    this.item.sellIn = this.item.sellIn - 1;
+  }
+
+  protected increaseQuality(): void {
+    this.item.quality = this.item.quality + 1;
+  }
+
+  protected decreaseQuality(): void {
+    this.item.quality = this.item.quality - 1;
+  }
+
+  protected hasExpired(): boolean {
+    return this.item.sellIn < 0;
+  }
+}
+
 export class GildedRose {
   items: Array<Item>;
 
