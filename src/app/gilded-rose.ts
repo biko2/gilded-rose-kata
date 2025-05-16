e80438e1dc9811e29a10f9f9599f2af8e71fe4e6
Endika Orube <endikaorve@gmail.com>
2025-05-16 11:19:27 +0200

    refactor: invierte condición del if para Backstage passes en updateQuality2

--- src/app/gilded-rose.ts
+++ src/app/gilded-rose.ts
@@ -77,12 +77,13 @@ const updateQuality2 = (item: Item): void => {
 
   if (item.sellIn < 0) {
     if (item.name != "Aged Brie") {
-      if (item.name != "Backstage passes to a TAFKAL80ETC concert") {
-        if (item.quality > 0) {
-          item.quality = item.quality - 1;
-        }
-      } else {
+      if (item.name === "Backstage passes to a TAFKAL80ETC concert") {
         item.quality = item.quality - item.quality;
+        return;
+      }
+
+      if (item.quality > 0) {
+        item.quality = item.quality - 1;
       }
     } else {
       if (item.quality < 50) {
