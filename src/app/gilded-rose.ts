de13e6f25f20b30df0355c2365281e3e319b2404
Endika Orube <endikaorve@gmail.com>
2025-05-16 11:41:09 +0200

    refactor: simplifica validación de calidad máxima en Backstage passes

--- src/app/gilded-rose.ts
+++ src/app/gilded-rose.ts
@@ -43,20 +43,18 @@ export class GildedRose {
           return;
         }
 
-        if (item.quality < 50) {
+        item.quality = item.quality + 1;
+
+        if (item.sellIn < 10) {
           item.quality = item.quality + 1;
+        }
 
-          if (item.sellIn < 10) {
-            if (item.quality < 50) {
-              item.quality = item.quality + 1;
-            }
-          }
+        if (item.sellIn < 5) {
+          item.quality = item.quality + 1;
+        }
 
-          if (item.sellIn < 5) {
-            if (item.quality < 50) {
-              item.quality = item.quality + 1;
-            }
-          }
+        if (item.quality > 50) {
+          item.quality = 50;
         }
 
         return;
