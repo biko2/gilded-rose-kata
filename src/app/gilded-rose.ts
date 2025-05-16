58a2f391ba31bab3e9fdb7976921a8ac6463d35f
Endika Orube <endikaorve@gmail.com>
2025-05-16 11:41:54 +0200

    refactor: simplifica validación de calidad mínima en ítems comunes

--- src/app/gilded-rose.ts
+++ src/app/gilded-rose.ts
@@ -62,16 +62,14 @@ export class GildedRose {
 
       item.sellIn = item.sellIn - 1;
 
-      if (item.quality <= 0) {
-        return;
-      }
-
       item.quality = item.quality - 1;
 
       if (item.sellIn < 0) {
-        if (item.quality > 0) {
-          item.quality = item.quality - 1;
-        }
+        item.quality = item.quality - 1;
+      }
+
+      if (item.quality < 0) {
+        item.quality = 0;
       }
     });
 
