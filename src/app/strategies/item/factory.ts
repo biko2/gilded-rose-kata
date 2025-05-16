75cf14d6d14f86bfde473cdfd597f218f69b8151
Endika Orube <endikaorve@gmail.com>
2025-05-16 12:27:44 +0200

    chore: espaciado

--- src/app/strategies/item/factory.ts
+++ src/app/strategies/item/factory.ts
@@ -9,11 +9,14 @@ export function createItemStrategy(item: Item): ItemStrategy {
   if (item.name === "Sulfuras, Hand of Ragnaros") {
     return new SulfurasStrategy(item);
   }
+
   if (item.name === "Aged Brie") {
     return new AgedBrieStrategy(item);
   }
+
   if (item.name === "Backstage passes to a TAFKAL80ETC concert") {
     return new BackstagePassStrategy(item);
   }
+
   return new CommonItemStrategy(item);
 }
