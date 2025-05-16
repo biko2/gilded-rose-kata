8a449c402a40b3cff6628559dddb40d2a4e0f297
Endika Orube <endikaorve@gmail.com>
2025-05-16 11:40:19 +0200

    refactor: reordena verificación de sellIn < 0 en Backstage passes

--- src/app/gilded-rose.ts
+++ src/app/gilded-rose.ts
@@ -38,6 +38,11 @@ export class GildedRose {
       if (item.name === "Backstage passes to a TAFKAL80ETC concert") {
         item.sellIn = item.sellIn - 1;
 
+        if (item.sellIn < 0) {
+          item.quality = 0;
+          return;
+        }
+
         if (item.quality < 50) {
           item.quality = item.quality + 1;
 
@@ -52,10 +57,6 @@ export class GildedRose {
               item.quality = item.quality + 1;
             }
           }
-        }
-
-        if (item.sellIn < 0) {
-          item.quality = item.quality - item.quality;
         }
 
         return;
