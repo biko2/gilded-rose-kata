685d9a761ac4989930bad116924eb153b4e3a408
Endika Orube <endikaorve@gmail.com>
2025-05-16 11:31:14 +0200

    refactor: unifica lógica de item común y elimina updateQuality2

--- src/app/gilded-rose.ts
+++ src/app/gilded-rose.ts
@@ -17,7 +17,6 @@ export class GildedRose {
     this.items.forEach((item) => {
       updateSellIn(item);
       updateQuality1(item);
-      updateQuality2(item);
     });
 
     return this.items;
@@ -72,6 +71,12 @@ const updateQuality1 = (item: Item): void => {
   }
 
   item.quality = item.quality - 1;
+
+  if (item.sellIn < 0) {
+    if (item.quality > 0) {
+      item.quality = item.quality - 1;
+    }
+  }
 };
 
 const updateSellIn = (item: Item): void => {
@@ -80,24 +85,4 @@ const updateSellIn = (item: Item): void => {
   }
 
   item.sellIn = item.sellIn - 1;
-};
-
-const updateQuality2 = (item: Item): void => {
-  if (item.name === "Sulfuras, Hand of Ragnaros") {
-    return;
-  }
-
-  if (item.name === "Aged Brie") {
-    return;
-  }
-
-  if (item.name === "Backstage passes to a TAFKAL80ETC concert") {
-    return;
-  }
-
-  if (item.sellIn < 0) {
-    if (item.quality > 0) {
-      item.quality = item.quality - 1;
-    }
-  }
 };
