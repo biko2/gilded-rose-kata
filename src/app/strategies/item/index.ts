512c1c5bc7a5a9818995c32375207b05d10ed2d2
Endika Orube <endikaorve@gmail.com>
2025-06-10 17:47:52 +0200

    chore: elimina archivos viejos

--- src/app/strategies/item/index.ts
+++
@@ -1,6 +0,0 @@
-export * from "./_strategy";
-export * from "./common";
-export * from "./sulfuras";
-export * from "./aged-brie";
-export * from "./backstage-pass";
-export * from "./_factory";
--- src/app/strategies/item/sulfuras.ts
+++
@@ -1,7 +0,0 @@
-import { ItemStrategy } from "./_strategy";
-
-export class SulfurasStrategy extends ItemStrategy {
-  update(): void {
-    // Sulfuras is legendary and never changes
-  }
-}
